4b42bf8f5e34f80f65e41544c5e5ecca29eae02f
harrisonhall <harrisonchristianhall@gmail.com>
2021-01-30 18:38:18 -0500

    datastax working

--- backend/src/datastax.py
+++ backend/src/datastax.py
@@ -1,46 +1,119 @@
 from passlib.hash import sha256_crypt
+import requests
+import json
 
 salt = "!LINKEDIN"
 default_website = "https://www.google.com"
 
-data = {} # volatile!
-websites = {}
-connections = {}
+dbid = "36978354-cbaf-430d-8995-12d491564a45"
+rid = "us-east1"
+ksid = "e_link"
 
-def username_free(username):
-    return username not in data
+url = f"https://{dbid}-{rid}.apps.astra.datastax.com/api/rest/v1/auth"
+headers = {
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+    "X-Cassandra-Request-Id":"7d86a16e-9407-4089-9f8c-cb968e89b2cc"
+}
+body = {
+    "username":"e_link",
+    "password":"Z8f7*yFvZVoPWYsqLN8q"
+}
+response = requests.post(url, headers=headers, data=json.dumps(body))
+auth_token = response.json()["authToken"]
+
+
+def username_free(user):
+    url = f"https://{dbid}-{rid}.apps.astra.datastax.com/api/rest/v2/keyspaces/{ksid}/users/{user}"
+    headers = {
+        "Accept": "application/json",
+        "X-Cassandra-Token": auth_token,
+    }
+    response = requests.request("GET", url, headers=headers)
+    return response.json()["count"] == 0
 
 def has_account(username):
     return not username_free(username)
 
-def make_account(username, password):
-    assert username not in data, "ERROR! username in data"
-    data[username] = sha256_crypt.encrypt(salt+password+salt)
-    websites[username] = default_website
+def make_account(username, password, email, website):
+    url = f"https://{dbid}-{rid}.apps.astra.datastax.com/api/rest/v2/keyspaces/{ksid}/users/"
+    headers = {
+        "Accept": "application/json",
+        "X-Cassandra-Token": auth_token,
+    }
+    data = {
+        "username": username,
+        "password": password,
+        "email": email,
+        "website": website
+    }
+    response = requests.request("POST", url, headers=headers, data=json.dumps(data))
     return True
 
 def check_credentials(username, password):
-    enc_pass = data.get(username, "")
-    return sha256_crypt.verify(salt+password+salt, enc_pass)
+    url = f"https://{dbid}-{rid}.apps.astra.datastax.com/api/rest/v2/keyspaces/{ksid}/users/{username}"
+    headers = {
+        "Accept": "application/json",
+        "X-Cassandra-Token": auth_token,
+    }
+    response = requests.request("GET", url, headers=headers)
+    return sha256_crypt.verify(salt+password+salt, response.json()["data"][0]["password"])
 
 def set_website(username, website):
-    websites[username] = website
+    url = f"https://{dbid}-{rid}.apps.astra.datastax.com/api/rest/v2/keyspaces/{ksid}/users/{username}"
+    headers = {
+        "Accept": "application/json",
+        "X-Cassandra-Token": auth_token,
+    }
+    data = {
+        "website": website
+    }
+    response = requests.request("PATCH", url, headers=headers, data=json.dumps(data))
+    return response.json()
 
 def get_website(username):
-    return websites.get(username, default_website)
+    url = f"https://{dbid}-{rid}.apps.astra.datastax.com/api/rest/v2/keyspaces/{ksid}/users/{username}"
+    headers = {
+        "Accept": "application/json",
+        "X-Cassandra-Token": auth_token,
+    }
+    response = requests.request("GET", url, headers=headers)
+    return response.json()["data"][0]["website"]
 
-def add_connection(username1, username2):
-    if username2 not in connections.get(username1, []):
-        connections[username1] = connections.get(username1, []).append(username2)
-        return True
-    return False
+def add_connection(user1, user2):
+    url = f"https://{dbid}-{rid}.apps.astra.datastax.com/api/rest/v2/keyspaces/{ksid}/connections/"
+    headers = {
+        "Accept": "application/json",
+        "X-Cassandra-Token": auth_token,
+    }
+    data = {
+        "user1": user1,
+        "user2": user2,
+    }
+    response = requests.request("POST", url, headers=headers, data=json.dumps(data))
+    return True
 
-def remove_connection(username1, username2):
-    if username2 in connections.get(username1, []):
-        connections[username1].remove(username2)
+def remove_connection(user1, user2):
+    url = f"https://{dbid}-{rid}.apps.astra.datastax.com/api/rest/v2/keyspaces/{ksid}/connections/{user1}/{user2}"
+    headers = {
+        "Accept": "application/json",
+        "X-Cassandra-Token": auth_token,
+    }
+    data = {
+        "user1": user1,
+        "user2": user2,
+    }
+    response = requests.request("DELETE", url, headers=headers, data=json.dumps(data))
+    return True
 
 def get_connections(username):
-    return connections.get(username, [])
+    url = f"https://{dbid}-{rid}.apps.astra.datastax.com/api/rest/v2/keyspaces/{ksid}/connections/{user}"
+    headers = {
+        "Accept": "application/json",
+        "X-Cassandra-Token": auth_token,
+    }
+    response = requests.request("GET", url, headers=headers)
+    return [x['user2'] for x in response.json()["data"]]
 
 def change_connection(username1, username2):
     if username2 not in get_connections(username1):
