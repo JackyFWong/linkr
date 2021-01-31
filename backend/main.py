96da84a3d14a679a9e0a0a9660f5176a4730d236
Jacky Wong <jwong2398@gmail.com>
2021-01-30 19:54:46 -0500

    creation of pages, initial layout

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,50 @@ app.secret_key = b"elink_secret_key"
 @app.route("/")
 def index():
     return render_template(
+        "pages/index.html",
+        context={}
+    )
+
+@app.route("/signup")
+def signup():
+    return render_template(
         "pages/signup.html",
+        context={}
+    )
+
+@app.route("/profile")
+def profile():
+    return render_template(
+        "pages/profile.html",
+        context={
+            'full_name': 'Bob Jones',
+            'personal_website': 'bobjones.com',
+            'picture_src': 'https://cdn.pixabay.com/photo/2021/01/06/21/50/couple-5895728_960_720.jpg',
+            'connections': {
+                'User One': 'userone.com',
+                'User Two': 'usertwo.com',
+                'User Three': 'userthree.com'
+                }
+            }
+    )
+
+@app.route("/customize")
+def customize():
+    return render_template(
+        "pages/customize.html",
+        context={
+            'full_name': 'Bob Jones',
+            'email': 'hi@bobjones.com',
+            'picture_src': 'https://cdn.pixabay.com/photo/2021/01/06/21/50/couple-5895728_960_720.jpg',
+            'personal_website': 'bobjones.com',
+            'theme': 'Light'
+        }
+    )
+
+@app.route("/widget")
+def widget():
+    return render_template(
+        "pages/widget.html",
         context={}
     )
 
